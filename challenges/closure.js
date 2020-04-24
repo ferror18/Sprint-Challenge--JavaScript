4ae298b3fb7dbc12123449617d7a1c2937c5b4e5
manuel <me@example.com>
2020-04-24 09:56:11 -0700

    classes done MVP + strech

--- challenges/closure.js
+++ challenges/closure.js
@@ -23,11 +23,11 @@ myFunction();
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
 // IF SUMMATION DOESNT REQUIRE CLOSURE
-// function summation (num){
-//   let counter = 0
-//   for (let i=0; i<num; i++){
-//     counter += num - i
-//   }
-//   return counter;
-// }
+function summation (num){
+  let counter = 0
+  for (let i=0; i<num; i++){
+    counter += num - i
+  }
+  return counter;
+}
 // console.log(summation(4))
